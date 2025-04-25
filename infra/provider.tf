3b6e0ac2b9aa142d40834a308d981ce330c2c631
Kelvin D. Alcala <kalcala@bsc.com.do>
2025-04-25 10:43:06 -0400

    se agrega kubeconfig a ingress

--- infra/provider.tf
+++ infra/provider.tf
@@ -35,4 +35,12 @@ provider "azuread" {
   
 }
 
-
+# Referenciando el output del módulo AKS
+provider "helm" {
+  kubernetes {
+    host                   = module.aks.kube_config["host"]
+    client_certificate     = base64decode(module.aks.kube_config["client_certificate"])
+    client_key             = base64decode(module.aks.kube_config["client_key"])
+    cluster_ca_certificate = base64decode(module.aks.kube_config["cluster_ca_certificate"])
+  }
+}
