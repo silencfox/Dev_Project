a8c802787c2b16d12ef85572c034b14548ee1c61
Kelvin AG <kalcala@bsc.com.do>
2025-05-07 22:54:42 -0400

    Update variables.tf

--- infra/modules/ablobs/variables.tf
+++ infra/modules/ablobs/variables.tf
@@ -19,3 +19,55 @@ variable "storeacount" {
   description = "storage account name"
   default = "tfdevsublobstorage007011"
 }
+
+variable "rgname" {
+  type        = string
+  description = "resource group name"
+
+}
+
+
+
+variable "service_principal_name" {
+  type = string
+}
+
+variable "keyvault_name" {
+  type = string
+}
+
+
+variable "aks_name" {
+  type = string
+}
+
+variable "sku" {
+  type = string
+}
+variable "acr_name" {
+  type = string
+}
+
+variable "create_acr" {
+  type    = bool
+  default = true
+}
+
+variable "ghpathfile" {
+  type = string
+}
+
+variable "TF_VAR_ghtoken" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+  description = "Ambiente de la infra Dev, QA, Prod"
+}
+
+variable "cluster_name" {
+  type = string
+  default = "Devsu-aks-cluster"
+  description = "Nombre del cluster AKS"
+}
