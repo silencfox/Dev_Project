e640dcccc9a9bb90cb7aa7cd6f5cfee5bbecb2b0
Kelvin AG <kalcala@bsc.com.do>
2025-05-07 22:57:08 -0400

    Update variables.tf

--- infra/modules/ablobs/variables.tf
+++ infra/modules/ablobs/variables.tf
@@ -30,22 +30,28 @@ variable "rgname" {
 
 variable "service_principal_name" {
   type = string
+  default = ""
 }
 
 variable "keyvault_name" {
   type = string
+  default = ""
 }
 
 
 variable "aks_name" {
   type = string
+  default = ""
 }
 
 variable "sku" {
   type = string
+  default = ""
 }
+
 variable "acr_name" {
   type = string
+  default = ""
 }
 
 variable "create_acr" {
@@ -55,14 +61,17 @@ variable "create_acr" {
 
 variable "ghpathfile" {
   type = string
+  default = ""
 }
 
 variable "TF_VAR_ghtoken" {
   type = string
+  default = ""
 }
 
 variable "environment" {
   type = string
+  default = ""
   description = "Ambiente de la infra Dev, QA, Prod"
 }
 
