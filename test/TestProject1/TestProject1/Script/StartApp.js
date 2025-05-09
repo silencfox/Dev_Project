68bcf3b693bf44cadb795c09485281614e385da2
Kelvin Alcala Grassal <kalcala@bsc.com.do>
2025-05-09 01:15:39 -0400

    add scripts detach

--- test/TestProject1/TestProject1/Script/StartApp.js
+++ test/TestProject1/TestProject1/Script/StartApp.js
@@ -5,11 +5,11 @@
     Log.Message("Ruta recibida: " + ruta);
 
     // Creamos el comando con cambio de directorio y npm install
-    var command = 'cmd.exe /c cd /d "' + ruta + '" && npm install &&  npm run test';
+    var command = 'cmd.exe /c cd /d "' + ruta + '" && npm install && npm run start';
 
     // Ejecutamos el comando
     var shell = Sys.OleObject("WScript.Shell");
-    shell.Run(command, 1, true); // 1 es para mostrar la ventana de CMD, true para esperar que termine
+    shell.Run(command, 1, false); // 1 es para mostrar la ventana de CMD, true para esperar que termine
   } else {
     Log.Error("No se recibió ruta desde la línea de comando.");
   }
