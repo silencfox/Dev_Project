244a630e4a38d8ac5bc52d383b879bec53ebb76e
Kelvin Alcala Grassal <kalcala@bsc.com.do>
2025-05-13 02:40:27 -0400

    mod argo CD app

--- deployment/argocd/install.ps1
+++ deployment/argocd/install.ps1
@@ -7,6 +7,7 @@ helm install argocd argo/argo-cd -n argocd
 #kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d echo
 
 ## pwsh
+Start-Sleep 120
 $secret = kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}"
 $password = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($secret))
 Write-Host "Contraseña de admin: $password"
@@ -18,3 +19,6 @@ kubectl port-forward svc/argocd-server -n argocd 8080:443
 #  --set server.service.type=LoadBalancer
 
 helm upgrade argocd argo/argo-cd -n argocd -f values.yaml
+
+## INSTALA APP LUEGO DE 
+helm upgrade --install argocd argo/argo-cd -n argocd --create-namespace -f values_app.yaml
