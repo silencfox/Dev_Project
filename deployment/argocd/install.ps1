b08b743712a52aecf027f3fbcbbc23ce342fa274
Kelvin Alcala Grassal <kalcala@bsc.com.do>
2025-05-13 02:03:51 -0400

    ADD argo CD

---
+++ deployment/argocd/install.ps1
@@ -0,0 +1,20 @@
+kubectl create namespace argocd
+helm repo add argo https://argoproj.github.io/argo-helm
+helm repo update
+helm install argocd argo/argo-cd -n argocd
+
+## sh
+#kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d echo
+
+## pwsh
+$secret = kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}"
+$password = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($secret))
+Write-Host "Contraseña de admin: $password"
+
+## solo en minikube
+kubectl port-forward svc/argocd-server -n argocd 8080:443
+
+#helm upgrade argocd argo/argo-cd -n argocd `
+#  --set server.service.type=LoadBalancer
+
+helm upgrade argocd argo/argo-cd -n argocd -f values.yaml
