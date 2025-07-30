7274cb7ebc7fb85da64de3c1a2fd998e41fedd69
Kelvin Alcala <kelvin.ag89@gmail.com>
2025-07-30 06:21:45 -0400

    fix minor

--- infraestructura/modules/add-ons/main.tf
+++ infraestructura/modules/add-ons/main.tf
@@ -39,7 +39,6 @@ resource "helm_release" "cert_manager" {
 
 
 
-<<<<<<< HEAD
 ####################
 
 resource "helm_release" "nginx_ingress" {
@@ -53,17 +52,4 @@ resource "helm_release" "nginx_ingress" {
 
   values = [file("${path.module}/values/ingress.yaml")]
 
-=======
-
-resource "helm_release" "external_nginx" {
-  name = "external"
-
-  repository       = "https://kubernetes.github.io/ingress-nginx"
-  chart            = "ingress-nginx"
-  namespace        = "ingress"
-  create_namespace = true
-  version          = "4.8.0"
-
-  values = [file("${path.module}/values/ingress.yaml")]
->>>>>>> a45e0a35053071caaf9b3d3a041d4da1b3b39467
 }
